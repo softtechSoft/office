f210af6b49c7e846d39c9427d510c5421f7a7d4a
softtechSoft <65577291+softtechSoft@users.noreply.github.com>
2020-05-19 11:45:06 +0900

    Add files via upload

---
+++ src/main/java/softtech/office/dao/SararyInfoDao.java
@@ -0,0 +1,8 @@
+package softtech.office.dao;
+
+import softtech.office.bean.SalaryinfoBean;
+import softtech.office.bean.SalaryinfoSelectBean;
+
+public interface SararyInfoDao {
+	SalaryinfoBean getSalaryInfo(SalaryinfoSelectBean para);
+}
